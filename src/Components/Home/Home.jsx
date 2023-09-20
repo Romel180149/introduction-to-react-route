a4c4a20a55a30ee01715961a4a11f7979350f974
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-09-20 11:30:18 +0600

    display navvar

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+           {/* <h2>This is home component</h2>  */}
+           {/* <Outlet></Outlet> */}
+        </div>
+    );
+};
+
+export default Home;

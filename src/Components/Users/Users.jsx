9a37c4ec79ff2c1e4c4c2a2f75ccb7efb5987e29
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-09-20 12:08:59 +0600

    display api

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,10 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+import User from "../User/User";
 
 
-const Users = () => {
+const Users = ({user}) => {
+    const users = useLoaderData()
+    // console.log(users);
     return (
         <div>
-          <h1>Our users</h1> 
+          <h1>Our users:{users.length}</h1> 
           <p>Fantastic and vodro users</p> 
+          <div>
+           {
+            users.map(user=><User key={user.id} user={user}></User>)
+           }
+          </div>
         </div>
     );
 };

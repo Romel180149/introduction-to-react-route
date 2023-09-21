e67463542ce851890e45b65d5a7767457211d927
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-09-21 09:54:59 +0600

    display navigate

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const Post = ({post}) => {
 const{id,title} = post;
+const navigate = useNavigate()
 const PostStyle = {
     border: '2px solid yellow',
     padding: '10px',
     borderRadius:'20px'
+}
+const handleShowDetails = () =>{
+  navigate(`/post/${id}`);
 }
     return (
         <div style={PostStyle}>
@@ -14,6 +18,7 @@ const PostStyle = {
             <p>{title}</p>
             <Link to={`/post/${id}`}>Post Detail</Link>
             <Link to={`/post/${id}`}><button>ShowDetails</button></Link>
+            <button onClick={handleShowDetails} >Click me see details</button>
         </div>
     );
 };
--- src/Components/PostDetails/PostDetails.jsx
+++ src/Components/PostDetails/PostDetails.jsx
@@ -1,14 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const PostDetails = () => {
     const post = useLoaderData();
+    const navigate = useNavigate()
     const{id,title,body}= post;
+    const handleGoBack = ()=>{
+    navigate(-1)
+    }
     return (
         <div>
             <h1>Post details about it: {id}</h1>
             <p>Title:{title}</p>
             <p><small>{body}</small></p>
+            <button onClick={handleGoBack}>Go back</button>
         </div>
     );
 };
